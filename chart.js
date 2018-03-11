ab874f81aa32360dbec5209d37ace90ea46f255d
Chrysa Tsiv <p15tsiv@ionio.gr>
2018-03-11 17:04:20 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 
-var fill = d3.scale.ordinal().range(["#F02233", "#087FBD", "#FDBB30"]);
+var fill = d3.scale.ordinal().range(["#177055", "#91323a", "#013baf"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
