130d5cbfbeb3e3603f06a1492b49ad942d068332
viktoriakal <viktoriakal64@gmail.com>
2018-03-14 23:34:49 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -102,7 +102,7 @@ function start() {
 		.style("fill", function(d) { return fill(d.party); })
 		.on("mouseover", mouseover)
 		.on("mouseout", mouseout)
-	        on("click", function(d) { window.open("http://www.google.com/search?q=" + d.donor);});
+	        .on("click", function(d) { window.open("http://www.google.com/search?q=" + d.donor);});
 		// Alternative title based 'tooltips'
 		// node.append("title")
 		//	.text(function(d) { return d.donor; });
